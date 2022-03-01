49ed30becaa2a456479557b68284d36b6239dc85
McTeaCup <69859445+McTeaCup@users.noreply.github.com>
2022-03-01 11:26:02 +0100

    Update InspectorItemShowcase.cs

--- Custom Editor Extention Documentation/Quick Install/InspectorItemShowcase.cs
+++ Custom Editor Extention Documentation/Quick Install/InspectorItemShowcase.cs
@@ -2120,7 +2120,7 @@ public class InspectorItemShowcase : EditorWindow
 
             using (new EditorGUILayout.VerticalScope())
             {
-                GUILayout.Label($"{stringCollection.d_IconNamesBigAndSmall[i]} ({i})");
+                GUILayout.Label($"{stringCollection.d_IconNamesBigAndSmall[i]}");
                 using (new EditorGUILayout.HorizontalScope())
                 {
                     if (GUILayout.Button("BIG", smallButtonStyle))
